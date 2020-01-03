b90abd22dfa3948532b94f8f7e0929b5fff0b13c
ulydev <ulydev@live.fr>
2020-01-03 20:08:47 +0100

    modify game initialisation and loop with states

--- source/main.c
+++ source/main.c
@@ -8,11 +8,22 @@
 #include <nds.h>
 #include <stdio.h>
 #include "game.h"
+#include "state_manager.h"
+
 int main(void) {
 
-	polling();
-	update_game();
-	draw();
-	while(1)
+	// initialize state manager
+	StateManager state_manager;
+	state_manager_init(&state_manager);
+
+	// push game state
+	state_manager_push(&state_manager, &STATE_GAME);
+
+	while (1) {
+		// game loop
+		polling();
+		state_manager_update(&state_manager);
+		state_manager_draw(&state_manager);
 		swiWaitForVBlank();
+	}
 }
