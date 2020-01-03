e3da7424de873d6cbfc6db6034b3c62ec7107ceb
ulydev <ulydev@live.fr>
2020-01-03 20:08:09 +0100

    move polling to input.h/c

---
+++ source/input.c
@@ -0,0 +1,15 @@
+/*
+ * input.c
+ *
+ *  Created on: Jan 3, 2020
+ *      Author: Ulysse
+ */
+
+#include "input.h"
+
+int poll_input() {
+
+    // TODO: poll key presses, touch...
+
+    return 0;
+}
---
+++ source/input.h
@@ -0,0 +1,12 @@
+/*
+ * input.h
+ *
+ *  Created on: Jan 3, 2020
+ *      Author: Ulysse
+ */
+
+/*!
+ * @brief Polls input
+ * @return 0 on success
+ */
+int poll_input();
