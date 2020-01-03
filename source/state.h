b0ceed88d7dd919dcc6d0b0f616e46500911867c
ulydev <ulydev@live.fr>
2020-01-03 20:19:59 +0100

    add init and deinit methods

--- source/state.h
+++ source/state.h
@@ -7,8 +7,10 @@
 #pragma once
 
 typedef struct {
-    // prevent subsequent update/draw calls for older states by returning anything != 0
+    // prevent subsequent calls for older states by returning anything != 0
     // for example, the pause menu state's update() will return 1 so that it pauses other states
+    int (*init)();
+    int (*deinit)();
     int (*update)();
 	int (*draw)();
 } State;
