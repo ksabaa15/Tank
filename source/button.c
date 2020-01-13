6981de7cb200e49801b06e1edfd1969882ea9a9f
ulydev <ulydev@live.fr>
2020-01-13 23:36:50 +0100

    check touch down in button

--- source/button.c
+++ source/button.c
@@ -5,9 +5,10 @@
  *      Author: Ulysse
  */
 #include "button.h"
+#include "input.h"
 
 void button_touch(Button *button, int x, int y) {
-    int touched = (
+    int touched = (down & KEY_TOUCH) && (
         (button->x <= x && x <= (button->x + button->width )) &&
         (button->y <= y && y <= (button->y + button->height))
     );
